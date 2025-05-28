ab2b8114761e6dc4df65d27f0b4dd2841ef97aef
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 19:03:29 +0530

    gg

---
+++ q2.py
@@ -0,0 +1,14 @@
+
+def largernumber(n1,n2):
+    if(n1>n2):
+        return(n1)
+    else:
+        return(n2)
+n1=int(input("enter number"))
+n2=int(input("enter number"))
+n3=int(input("enter number"))
+n4=int(input("enter number"))
+m1=largernumber(n1,n2)
+m2=largernumber(n3,n4)
+print(m1)
+print(m2)
---
+++ text3.py
@@ -0,0 +1,7 @@
+def birathdawish (name,age):
+    print("happy birath day",name)
+    print(f"you are now old{name}")
+birathdawish("fahath","23")
+birathdawish("amstada","99")
+x=sum(2,4)
+print(x)
